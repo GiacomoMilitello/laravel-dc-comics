f6a843caabe11fadd89d9795334740ad116f8ad1
GiacomoMilitello <g.militello95@gmail.com>
2024-03-20 14:56:33 +0100

    destroy

--- app/Http/Controllers/backend/ComicsController.php
+++ app/Http/Controllers/backend/ComicsController.php
@@ -71,6 +71,8 @@ class ComicsController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $comic = Comic::findOrFail($id);
+        $comic->delete();
+        return redirect()->route('comics.index');
     }
 }
--- resources/views/pages/comics/index.blade.php
+++ resources/views/pages/comics/index.blade.php
@@ -15,27 +15,37 @@
             <table class="table">
                 <thead>
                     <tr>
+                        <th scope="col">id</th>
                         <th scope="col">title</th>
-                        <th scope="col">description</th>
                         <th scope="col">thumb</th>
                         <th scope="col">price</th>
                         <th scope="col">series</th>
                         <th scope="col">sale_date</th>
                         <th scope="col">type</th>
+                        <th scope="col">actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($comics as $item)
                         <tr>
+                            <td>{{ $item->id }}</td>
                             <th scope="row">
                                 <a href="{{route('comics.show', $item->id)}}">{{$item->title}}</a>
                             </th>
-                            <td>{{ $item->description }}</td>
                             <td>{{ $item->thumb }}</td>
                             <td>{{ $item->price }}</td>
                             <td>{{ $item->series }}</td>
                             <td>{{ $item->sale_date }}</td>
                             <td>{{ $item->type }}</td>
+                            <td>
+                            <form action="{{ route('comics.destroy', $item->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+
+                                <button class="btn btn-danger" type="submit">ELIMINA</button>
+
+                            </form>
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
